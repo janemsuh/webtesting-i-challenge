54fb8972f6df535fce6ccad7aa472ab64c0797b3
Jane Suh <sourcherryyogurt@gmail.com>
2020-05-14 18:31:02 -0400

    Completed repair, succeed, and fail functions

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,28 @@ module.exports = {
 };
 
 function succeed(item) {
+  if (item.enhancement < 20) {
+    item.enhancement += 1;
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability -= 5;
+  } else if (item.enhancement > 16) {
+    item.enhancement -= 1;
+    item.durability -= 10;
+  } else {
+    item.durability -= 5;
+  }
+  item.enhancement = Math.max(0, item.enhancement);
+  item.durability = Math.max(0, item.durability);
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
